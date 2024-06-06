2cef12e8242bfc90cb9ae419cbda86e7a3486033
Lukas <larurau@gmail.com>
2024-06-06 18:30:17 +0200

    Refactored resizing to happen as early as possible

--- main.py
+++ main.py
@@ -23,21 +23,21 @@ def import_data(thousandths, reimport=True, random_reduction=False):
     games_df = pandas.read_csv(games_csv_path)[['BGGId', 'Name']]
     user_ratings_df = pandas.read_csv(user_ratings_csv_path)[['BGGId', 'Rating', 'Username']]
 
-    # Merge the dataframes on BGGId and select the necessary columns
-    merged_df = pandas.merge(user_ratings_df, games_df, on='BGGId')[['Username', 'Rating', 'Name']]
-    merged_df = merged_df.sort_values(by='Name')
-
-    # Reduce the size of the dataset
-    print(f'Size before reduction is: {merged_df.shape[0]}')
+    # Before merging reduce the size of the dataset
+    print(f'Size before reduction is: {games_df.shape[0]}')
     factor = thousandths / 1000
     if random_reduction:
         print("The reduction is achieved by random sampling")
-        merged_df = merged_df.sample(frac=factor, random_state=42)
+        games_df = games_df.sample(frac=factor, random_state=42)
     else:
         print("The reduction is achieved by taking the first elements of the dataframe")
-        x = int(len(merged_df) * factor)
-        merged_df = merged_df.head(x)
-    print(f'Size after reduction is: {merged_df.shape[0]}')
+        x = int(len(games_df) * factor)
+        games_df = games_df.head(x)
+    print(f'Size after reduction is: {games_df.shape[0]}')
+
+    # Merge the dataframes on BGGId and select the necessary columns
+    merged_df = pandas.merge(user_ratings_df, games_df, on='BGGId')[['Username', 'Rating', 'Name']]
+    merged_df = merged_df.sort_values(by='Name')
 
     # Calculate the average rating
     median_rating = user_ratings_df['Rating'].median()
@@ -46,8 +46,9 @@ def import_data(thousandths, reimport=True, random_reduction=False):
     # Replace 'Rating' with 'likes' if above median, 'does not like' if below or equal to the median
     merged_df['Rating'] = merged_df['Rating'].apply(lambda x: 'likes' if x >= median_rating else 'does not like')
 
+    print(f'Size of merged is: {merged_df.shape[0]}')
     only_likes = merged_df[merged_df['Rating'] == 'likes']
-    print(f'Size after only considering likes is: {only_likes.shape[0]}')
+    print(f'Size of merged after only considering likes is: {only_likes.shape[0]}')
 
     print('Finished Importing\n')
 
